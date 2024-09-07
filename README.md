70d0158f8e0da12f751c943b20f6a3eba99a0557
Julien Boz <julien.boz@gmail.com>
2024-09-07 11:52:51 +0200

    chore: 🗑️  clean

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # react-native-vlc-media-player-view
 
 React Native VLC media player view with full controls
+
+```tsx
+import { useEffect, useRef } from 'react';
+import { StyleSheet } from 'react-native';
+import { PlayerRef, VlcPlayer } from 'react-native-vlc-media-player-view';
+
+export default function App() {
+  const playerRef = useRef<PlayerRef>(null);
+
+  useEffect(() => {
+    const player = playerRef.current;
+    if (player) {
+      player.seekTo(50 * 1000);
+    }
+  }, [playerRef.current]);
+
+  return (
+    <VlcPlayer
+      ref={playerRef}
+      source={{ uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }}
+      style={styles.container}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+```
--- example/App.tsx
+++ example/App.tsx
@@ -3,29 +3,31 @@ import { StyleSheet } from 'react-native';
 import { PlayerRef, VlcPlayer } from 'react-native-vlc-media-player-view';
 
 export default function App() {
-  const mp4 = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
-  const mkv = 'http://apsmart.in/movie/545077210277743/1593574628/123115.mkv';
-  const direct = 'http://apsmart.in/545077210277743/1593574628/7809';
-
   const playerRef = useRef<PlayerRef>(null);
 
   useEffect(() => {
     const player = playerRef.current;
     if (player) {
-      player.play();
+      player.seekTo(50 * 1000);
     }
   }, [playerRef.current]);
 
-  return <VlcPlayer ref={playerRef} source={{ uri: direct, autoplay: false }} style={styles.container} />;
+  return (
+    <VlcPlayer
+      ref={playerRef}
+      source={{ uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }}
+      style={styles.container}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
-    height: '100%'
-    // backgroundColor: 'black',
-    // alignItems: 'center',
-    // justifyContent: 'center'
+    height: '100%',
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
