8cf98bb744b7690820e9b5fba9b5e32e022e191d
Julien Boz <julien.boz@gmail.com>
2024-09-17 20:58:13 +0200

    feat: 🚧 controls

--- example/App.tsx
+++ example/App.tsx
@@ -1,24 +1,28 @@
 import { MaterialIcons } from '@expo/vector-icons';
-import { View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useVideoPlayer, VideoView } from 'react-native-vlc-media-player-view';
 
 export default function App() {
-  const player = useVideoPlayer({}, player => {
-    player.source = { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' };
-    player.play();
-    player.title = 'Big Buck Bunny 1080p.mp4';
-  });
+  const player = useVideoPlayer();
+
+  const [uri, setUri] = useState<string>('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
+
+  useEffect(() => {
+    player.play({ uri });
+    player.title = 'Big Buck Bunny';
+  }, [uri]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+    <GestureHandlerRootView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
       <MaterialIcons name="play-circle-outline" size={0} color="black" />
       <VideoView
         player={player}
         onLoaded={e => console.log('loaded', e.nativeEvent)}
-        onBack={() => console.log('back')}
         onNext={() => console.log('next')}
         onPrevious={() => console.log('previous')}
+        onBack={() => console.log('back')}
       />
-    </View>
+    </GestureHandlerRootView>
   );
 }
